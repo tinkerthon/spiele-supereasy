eaf9ec9de0bc702e2f2cddd8fc4f3ec871974f33
Olav Schettler <olav@schettler.net>
2019-03-31 12:02:34 +0200

    First commit

---
+++ Obst/obstattacke.py
@@ -0,0 +1,22 @@
+import pgzrun
+from random import randint
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print ("Treffer!")
+        place_apple()
+    else:
+        print("Daneben!")
+        quit()
+        
+place_apple()        
+pgzrun.go()
---
+++ README.md
@@ -0,0 +1,1 @@
+Spiele aus dem Buch 'Spiele mit Python supereasy' von Carol Vordermann
